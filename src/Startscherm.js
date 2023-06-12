dc73a02534bcfdcb91ad00d2acaa3b9c9c10004e
FloriBanaan <79276561+FloriBanaan@users.noreply.github.com>
2023-06-12 14:30:03 +0200

    Create Startscherm.js

---
+++ src/Startscherm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Startscherm = () => {
+    return (
+        <div>
+            <h1> Stotter App </h1>
+            <button class="button3"> Start </button>
+            <figure>
+                <img src="bram.png" alt="Bram"></img>
+            </figure>
+        </div>
+    );
+};
+
+export default Startscherm;

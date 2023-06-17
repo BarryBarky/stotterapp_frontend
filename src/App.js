b496c59bc50ed61a6ea466238059d5952a789919
Tim Dullaart <77988256+Oriantar@users.noreply.github.com>
2023-06-18 01:32:39 +0200

    Update App.js to enable hopefully HTTPS :D

--- src/App.js
+++ src/App.js
@@ -4,8 +4,36 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Levels from "./pages/Levels";
 
-function App(){
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/oriantar.nl/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/oriantar.nl/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/oriantar.nl/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+app.use((req, res) => {
+  res.send('Hello HTTPS!');
+});
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () =>{
+  console.log('HTTP Server running on port 80');
+});
+httpsServer.listen(443, () =>{
+  console.log('HTTP Server running on port 443');
+});
+
+function App(){
+  
   return (
     <>
         <BrowserRouter>
